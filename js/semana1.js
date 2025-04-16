be897ed520475c8d4e7596c2e471ea2b5f5321cb
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-04-16 01:00:35 -0500

    FINALIZADA LA CLASE

--- js/semana1.js
+++ js/semana1.js
@@ -1,2 +1,55 @@
 console.log("Semana 1 grabación.")
 
+// variables 
+
+let nombre = "Johan giraldo";
+let edad = 34;
+let verdadero = true;
+
+nombre = "Alexis Duque"// reemplaza la variable 
+console.log(nombre);
+console.log(edad);
+console.log(verdadero);
+
+
+//contastantes 
+
+const comision = 70122;
+
+let curso = "Javascript";
+
+console.log(comision);
+console.log(curso);
+console.log("Esta es la comision "+comision +" es el curso de " + curso);
+
+// para escirbir los mensajes concatenar con la +
+
+// solicitar informacion 
+
+let name = prompt("Ingrese su nombre");
+let age = prompt("Ingrese la edad ");
+
+console.log("Mi nombre es " + name + " Mi edad es de " + age);
+// alert se usa para mostar en el navegador. 
+
+alert("Mi nombre es " + name + " Mi edad es de " + age);
+
+// 
+//duplicar alt shift flecha
+let numeroA = parseInt(prompt("Ingrese un numero A"));
+let numeroB = parseInt( prompt("Ingrese un numero B"));
+
+// DIVISION 
+let division = numeroA/numeroB;
+console.log(division);
+let multiplicacion = numeroA*numeroB;
+console.log(multiplicacion);
+
+let resta = numeroA - numeroB;
+console.log(resta);
+// tener cuidado con la suma
+
+//uso parseInt para convertir un prompt que es un str, a numero. 
+let suma = numeroA + numeroB
+console.log(suma)
+
