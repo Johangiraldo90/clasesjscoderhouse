f20143ad54778d0c0afbeeba156278d1acc7faa9
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-04-19 14:56:21 -0500

    Subiendo lo de la clase 3

--- js/semana3-clase.js
+++ js/semana3-clase.js
@@ -1,1 +1,47 @@
 console.log("Clase grabada semana 3")
+
+// function saludo(){
+//     console.log("Hola coder House")
+// }
+
+// saludo()
+
+// function sumar(){
+//     let numeroA = 12;
+//     let numeroB = 23;
+//     let resultado = numeroA+numeroB;
+//     console.log(resultado);
+// }
+
+// sumar()
+
+// function sumar(){
+//     let numeroA = 12;
+//     let numeroB = 23;
+//     let resultado = numeroA+numeroB;
+//     return resultado;
+// }
+
+// console.log(sumar())
+
+// function sumar(){
+//     let numeroA = 12;
+//     let numeroB = 23;
+//     let resultado = numeroA+numeroB;
+//     return resultado;
+// }
+// let calculo = sumar()
+// let ConIva = calculo*1.19
+// console.log(ConIva) /// funcion declarada 
+
+// funcion expresada 
+
+const restar = function (){
+    let numeroA = 15;
+    let numeroB = 20;
+    let resultado = numeroB - numeroA;
+
+    console.log(resultado)
+}
+
+restar()// siempre se llama primero la funcion luego se llama 
