da49b79c04c29b990bea80083aeab54023e81b7d
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-04-19 15:57:07 -0500

    Funcion flecha

--- js/semana3-clase.js
+++ js/semana3-clase.js
@@ -36,12 +36,42 @@ console.log("Clase grabada semana 3")
 
 // funcion expresada 
 
-const restar = function (){
-    let numeroA = 15;
-    let numeroB = 20;
-    let resultado = numeroB - numeroA;
+// const restar = function (){
+//     let numeroA = 15;
+//     let numeroB = 20;
+//     let resultado = numeroB - numeroA;
 
-    console.log(resultado)
-}
+//     console.log(resultado)
+// }
 
-restar()// siempre se llama primero la funcion luego se llama 
+// restar()// siempre se llama primero la funcion luego se llama 
+
+// let primerNumero = parseInt(prompt("Ingrese primero Numero"))
+// let segundoNumero = parseInt(prompt("Ingrese segundo Numero"))
+
+// function sumar (numeroA, numeroB){
+//     let resultado = numeroA + numeroB
+//     console.log(resultado)
+// }
+
+// sumar(primerNumero,segundoNumero)
+
+// const caluculadoradel10 = function(primerNumero){
+//     let resultado = primerNumero*10
+//     console.log(resultado)
+// }
+
+// caluculadoradel10(2)
+
+// for( i = 0; i<=10; i++){
+//     caluculadoradel10(i);
+// }
+
+
+//funcion flecha con un solo parametro 
+// const caluculadoradel10 = primerNumero=> primerNumero*10
+    
+// console.log(caluculadoradel10(2))
+
+const multiplicacion = (numeroA, NumeroB)=>numeroA*NumeroB
+console.log(multiplicacion(2, 4))
